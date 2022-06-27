e06b598913ddb962528b360c0e40b996b7af0561
Stepan <St3Ba4@gmail.com>
2022-06-27 19:54:50 +0300

    rename QueenPlaceholder

--- program.java
+++ program.java
@@ -3,9 +3,9 @@ package Queen;
 public class Program {
 	public static void main(String[] args) {
 		int sizeBoard = 8;
-		QueenPlaceholder qp = new QueenPlaceholder(sizeBoard);
-		qp.findPlacement();
-		System.out.println(qp.repository.read());
-		System.out.println("Всего: " + qp.repository.getPlacementList().size() + " возможные расстановоки");
+		QueenPlaceholder placeholder = new QueenPlaceholder(sizeBoard);
+		placeholder.findPlacement();
+		System.out.println(placeholder.repository.read());
+		System.out.println("Всего: " + placeholder.repository.getPlacementList().size() + " возможные расстановоки");
 	}
 }
